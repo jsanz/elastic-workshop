e2c4625a80f40b34704a20e50730d7dca21fde8f
Jorge Sanz <jorge.sanz@elastic.co>
2020-02-04 19:26:53 +0100

    same with the opensky config

--- lab/opensky-loader/config/index.js
+++ lab/opensky-loader/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     es_config: {
-	node: 'http://elasticsearch:9200/',
+        node: 'http://wecode-elasticsearch:9200/',
     },
     index_name: 'flight_tracking',
     sleep_seconds: 60
