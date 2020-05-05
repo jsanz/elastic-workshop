c4d8e9e71ef44b9f753311a1ef6a5b3bfaed500e
Jorge Sanz <jorge.sanz@elastic.co>
2020-05-05 19:30:55 +0200

    added opensky-viewer to the compose serivces

--- lab/opensky-loader/config/index.js
+++ lab/opensky-loader/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     es_config: {
-        node: 'http://wecode-elasticsearch:9200/',
+        node: 'http://workshop-elasticsearch:9200/',
     },
     index_name: 'flight_tracking',
     sleep_seconds: 60
