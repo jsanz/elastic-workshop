c40fe702cab393dcbe925fe501aa86397840bd8c
Jorge Sanz <jorge@jorgesanz.net>
2022-05-30 16:24:23 +0200

    parametrize password for opensky apps

--- lab/elastic-config.js
+++ lab/elastic-config.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 
+const password = process.env.ELASTIC_PASSWORD || 'changeme';
+
 module.exports = {
     es_config: {
         node: 'https://es01:9200',
         auth: {
             username: 'elastic',
-            password: 'changeme'
+            password
         },
         tls: {
           ca: fs.readFileSync('/usr/share/app/certs/ca/ca.crt'),
